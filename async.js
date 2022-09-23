a8acea5f53d2f37871fcec7c3b12979601033213
abhiarnikar <abhiarnikar725@gmail.com>
2022-09-23 08:01:05 +0530

    Practice

---
+++ async.js
@@ -0,0 +1,11 @@
+console.log('hello')
+
+
+async function abhijit(){
+    return "abhi";
+}
+console.log('before calling abhi')
+let a = abhijit()
+console.log('after calling abhi')
+console.log(a)
+console.log('last line of the file')
