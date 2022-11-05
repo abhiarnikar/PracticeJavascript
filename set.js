f8e2fa547e45782c52e21efa7375d733b3cf521a
abhiarnikar <abhiarnikar725@gmail.com>
2022-11-05 15:51:47 +0530

    set

---
+++ set.js
@@ -0,0 +1,33 @@
+let array = new Set([11,25,14,11,25,63,78,25,63])
+console.log(array)
+
+// set does not store the value by index and 
+// does not store the double value
+
+let names= new Set('abhijitaa') // does not store the double value
+console.log(names)
+
+let persons = new Set()
+console.log(persons)
+
+//adding the properies
+
+persons.add('abhijit')
+persons.add('vishwa')
+persons.add('krushna')
+persons.add('kaivaly')
+persons.add('krushna')
+
+console.log(persons)
+
+console.log(persons.has('abhijit'))
+console.log(persons.has('krushna'))
+
+console.log(persons.size)
+
+persons.delete('abhijit')
+console.log(persons)
+
+persons.clear()
+
+console.log(persons)
