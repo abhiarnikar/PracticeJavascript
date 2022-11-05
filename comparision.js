87be6f730526a0d760761fcb00b1c6a84a286a33
abhiarnikar <abhiarnikar725@gmail.com>
2022-11-05 19:03:16 +0530

    comparator

---
+++ comparision.js
@@ -0,0 +1,27 @@
+// < > >= <= == === != !== ===
+
+// != means is not equal to
+
+// != compare only value
+// == compare only value
+
+// !== compare value and type
+// === compare value and type
+
+console.log(typeof 'abhijit')
+console.log(typeof 5)
+console.log(typeof '6')
+console.log(typeof true)
+
+console.log(6 == 6)
+console.log(6 =='6')
+console.log(6 == '5')
+console.log(7 != 6)
+console.log(7 != '6')
+console.log(7 != '7')
+console.log('************')
+console.log(5 === '5')
+console.log( 8 !== '9')
+console.log(10 === 10)
+console.log(5 !== 5)
+console.log(5 !== '5')
